9894b31d1ca0ff9293632f40cc1d15393ef3abcc
kimhab <69948342+kimhab11@users.noreply.github.com>
2022-11-01 10:15:13 +0700

    c

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-api-backend-kimhab'),
+      dir: require('path').join(__dirname, './coverage/angular-api-inmemory-kimhab'),
       subdir: '.',
       reporters: [
         { type: 'html' },
